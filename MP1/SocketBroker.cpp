4f666759c730b312c76d37a71000b623d7932273
moontails <dragontransformer@gmail.com>
2015-02-20 15:04:15 -0600

    source file for SocketBroker.h

--- MP1/SocketBroker.cpp
+++ MP1/SocketBroker.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <sys/socket.h>
 #include "headers/SocketBroker.h"
 
 int SocketBroker::add(int x, int y)
 {
   return x + y;
+}
+
+void* SocketBroker::SocketBrokerTCP(void *lp)
+{
+  
 }
